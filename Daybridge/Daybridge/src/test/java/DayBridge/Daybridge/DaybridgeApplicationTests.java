a9de138aea20ed7750676ea4c5eb0cfd87588a2f
hs-1771304-kukseungbin <86177125+hs-1771304-kukseungbin@users.noreply.github.com>
2024-03-14 21:21:06 +0900

    SpringBoot , Mysql 의존성 설정 완료

---
+++ Daybridge/Daybridge/src/test/java/DayBridge/Daybridge/DaybridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package DayBridge.Daybridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaybridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
