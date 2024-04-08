8680ca94e4446cdb5790fb236de5561990e87a77
hs-1771304-kukseungbin <86177125+hs-1771304-kukseungbin@users.noreply.github.com>
2024-04-08 10:20:43 +0900

    gpt, image 생성 완료

---
+++ DayBridge/DayBridge/src/main/java/com/example/DayBridge/config/AiServiceConfig.java
@@ -0,0 +1,33 @@
+package com.example.DayBridge.config;
+
+import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+
+@Configuration
+public class AiServiceConfig {
+
+    @Value("${openai.key}")
+    private String apiKey;
+
+    //gpt에 apikey전달
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+
+    @Bean
+    public HttpHeaders httpHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + apiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+}
---
+++ DayBridge/DayBridge/src/main/java/com/example/DayBridge/domain/AiData.java
@@ -0,0 +1,33 @@
+package com.example.DayBridge.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
+public class AiData {
+
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private Long id;
+
+    String prompt;
+    // 색상
+    String color;
+    // 가구
+    String furniture;
+    // 창문의 개수
+    int window;
+    //방 평수 <-?
+    int width;
+    // 기타 고려사항
+    String etc;
+}
