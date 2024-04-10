be85a0ebf1400a22968a04508e4334d0b9a9f5be
hs-2191145-ParkYujin <118151172+blmngtgr@users.noreply.github.com>
2024-04-10 19:31:30 +0900

    Update FormRepository.java

--- DayBridge/DayBridge/src/main/java/com/example/DayBridge/repository/FormRepository.java
+++ DayBridge/DayBridge/src/main/java/com/example/DayBridge/repository/FormRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FormDataRepository extends JpaRepository<Interior, Long> {
+public interface FormDataRepository extends JpaRepository<FormData, Long> {
 
     List<FormData> findByUserNo(Long userNo); // 사용자별 생성된 이미지 출력
 
