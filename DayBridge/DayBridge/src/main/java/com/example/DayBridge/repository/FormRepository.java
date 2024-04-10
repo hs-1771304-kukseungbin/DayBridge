2a021c244f2180da3fabeee8e57c12bf8abdce95
hs-2191145-ParkYujin <118151172+blmngtgr@users.noreply.github.com>
2024-04-10 19:28:33 +0900

    FormRepository 추가 완료

---
+++ DayBridge/DayBridge/src/main/java/com/example/DayBridge/repository/FormRepository.java
@@ -0,0 +1,13 @@
+package com.example.DayBridge.repository;
+
+import com.example.DayBridge.model.Interior;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FormDataRepository extends JpaRepository<Interior, Long> {
+
+    List<FormData> findByUserNo(Long userNo); // 사용자별 생성된 이미지 출력
+
+
+}
