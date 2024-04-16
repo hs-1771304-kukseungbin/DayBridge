24fad2399b437477c0661714ae6a2f063be3dee8
hs-1771304-kukseungbin <86177125+hs-1771304-kukseungbin@users.noreply.github.com>
2024-04-16 16:37:18 +0900

    chatGPT 개발

--- DayBridge/DayBridge/src/main/java/com/example/DayBridge/controller/FormController.java
+++ DayBridge/DayBridge/src/main/java/com/example/DayBridge/controller/FormController.java
@@ -1,6 +1,8 @@
 package com.example.DayBridge.controller;
 
 import com.example.DayBridge.service.FormService;
+import io.github.flashvayne.chatgpt.dto.image.ImageFormat;
+import io.github.flashvayne.chatgpt.dto.image.ImageSize;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.io.IOException;
+
 @Controller
 public class FormController {
 
     @Autowired
     private FormService formService;
-
+    
     @RequestMapping("/form")
     public String showForm() {
         return "form"; // HTML 폼 페이지
@@ -26,14 +30,21 @@ public class FormController {
                              @RequestParam("windowNum") Integer windowNum,
                              @RequestParam("essentialFurniture") String essentialFurniture,
                              @RequestParam("roomSize") String roomSize,
-                             Model model) {
+                             Model model) throws IOException {
 
         formService.saveFormData(userNo, pointColor, windowPosition, windowNum, essentialFurniture, roomSize);
         String dataToSend = pointColor + "색의 톤에, 가구는" + essentialFurniture + "가 있고, 창문이" + windowNum + "개 있는," + roomSize+"평의 방";
 
         // chatgpt
+//        public void testImageList(){
+//    List<String> images = chatgptService.imageGenerate("A cute baby sea otter", 2, ImageSize.SMALL, ImageFormat.URL);
+//    System.out.print(images.toString());//["https://oaidalleapipr.....ZwA%3D","https://oaidalleapipr....RE0%3D"]
+//} 이런식으로 사진 사이즈랑 json형식으로 받아올 수 있음
 
-        model.addAttribute("dataToSend", dataToSend);
+        // 여기서 리턴 받는건 이미지 파일
+        byte[] image = formService.getImageResponse(dataToSend, ImageSize.LARGE, ImageFormat.BASE64);
+        // 이부분 수정됨
+        model.addAttribute("image", image);
 
         return "result"; // 결과 표시할 페이지
     }
--- DayBridge/DayBridge/src/main/java/com/example/DayBridge/domain/JoinRequest.java
+++ DayBridge/DayBridge/src/main/java/com/example/DayBridge/domain/JoinRequest.java
@@ -1,19 +1,26 @@
 package com.example.DayBridge.domain;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
 public class JoinRequest {
-    @NotBlank(message = "아이디를 입력해주세요")
+    @NotEmpty(message = "아이디를 입력해주세요")
     private String userID;
 
-    @NotBlank(message = "비밀번호를 입력해주세요")
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String email;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요")
     private String userPW;
     private String pwCheck;
 
-    @NotBlank(message = "사용하실 닉네임을 입력해주세요")
+    @NotEmpty(message = "사용하실 닉네임을 입력해주세요")
     private String nickName;
 
     public Users toEntity(String encode) {
@@ -21,6 +28,8 @@ public class JoinRequest {
                 .userID(this.userID)
                 .userPW(encode)
                 .nickName(this.nickName)
+                .email(this.email)
+                .name(this.name)
                 .build();
     }
 }
